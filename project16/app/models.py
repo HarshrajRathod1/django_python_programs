a265f3124f44182dc1c79eb42fe8c77ff14481cf
HarshrajRathod1 <harshrajrathod91@gmail.com>
2026-07-14 20:09:22 +0530

    add StudentManager in models.py

--- project16/app/models.py
+++ project16/app/models.py
@@ -1,16 +1,29 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.db.models import Q
 # Create your models here.
+
+class StudentManager(models.Manager):
+    def get_pass(self):
+        qs=Student.objects.all()
+        qs1=qs.filter(sub1__gte=40,sub2__gte=40)
+        return qs1
+    
+    def get_fail(self):
+        qs=Student.objects.all()
+        qs1=qs.filter(Q(sub1__lt=40)|Q(sub2__lt=40))
+        return qs1
+    
 
 class Student(models.Model):
     rollno=models.IntegerField(primary_key=True)
     name=models.CharField(max_length=20)
     sub1=models.FloatField()
     sub2=models.FloatField()
+    objects=StudentManager()
 
     def get_total(self):
         return self.sub1+self.sub2
-    
     def get_avg(self):
         return (self.sub1+self.sub2)//2
     
